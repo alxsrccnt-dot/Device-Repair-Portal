1ee17e5d4e74d57672d61b7791a78ce6ec9105ab
alxsrccnt-dot <alx.src.cnt@gmail.com>
2026-01-15 10:23:51 +0200

    Refactorig

---
+++ DeviceRepairPortal/Apis/Jobs/JobMonitoringApi/Program.cs
@@ -0,0 +1,38 @@
+using Application;
+using Carter;
+using Infrastructure;
+using JobManagementApi.Infrastructure;
+using MainApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddCarter();
+builder.Services.AddCustomAuthentification(builder.Configuration);
+builder.Services.AddWebServices();
+
+builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddInfrastucture(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapCarter();
+
+app.Run();
