3b636dc9634b08fd0053ebab3a1afba834fdcade
alxsrccnt-dot <alx.src.cnt@gmail.com>
2026-02-01 21:38:15 +0200

    refactor

--- DeviceRepairPortal/Apis/Jobs/Application/Monitoring/Common/DiscountDto.cs
+++ DeviceRepairPortal/Apis/Jobs/Application/Monitoring/Common/DiscountDto.cs
@@ -2,7 +2,7 @@
 
 public class DiscountDto
 {
-    public string Code { get; init; }
+    public required string Code { get; init; }
     public int Value { get; init; }
     public bool IsPercentage { get; init; }
 }
